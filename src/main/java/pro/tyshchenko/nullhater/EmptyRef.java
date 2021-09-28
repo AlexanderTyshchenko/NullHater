f5e53d23b501e388b541913846131e92feaa5e14
alexander.tyshchenko <alexander.tyshchenko@gmail.com>
2021-09-28 18:07:56 +0300

    refactoring

--- src/main/java/pro/tyshchenko/nullhater/EmptyRef.java
+++ src/main/java/pro/tyshchenko/nullhater/EmptyRef.java
@@ -4,6 +4,28 @@ package pro.tyshchenko.nullhater;
  */
 public final class EmptyRef {
 
+    /**
+     *
+     * Some situation
+     * <pre>{@code
+     * Object value = null; // here in as Object can be any class
+     *  // you always need to assign with null reference
+     *  // of course you can use Optional.ofNullable(), but exist situations
+     *  // when you need to use just reference and you truly OOP guy that hate
+     *  // null reference in code
+     * if (some condition) {
+     *     value = ... some value
+     * } else if (some condition) {
+     *     value = ... some value
+     * }
+     *}</pre>
+     * Usage
+     * <pre>{@code
+     * Object value = justEmpty();
+     * }</pre>
+     * @param <T>
+     * @return wrapped null reference
+     */
     public static <T> T justEmpty() {
         return null;
     }
