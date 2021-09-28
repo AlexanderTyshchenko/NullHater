7bffafc65ac3ca03f3d0b991046259be9be022d8
alexander.tyshchenko <alexander.tyshchenko@gmail.com>
2021-09-28 17:20:50 +0300

    refactoring

--- src/main/java/pro/tyshchenko/nullhater/EmptyRef.java
+++ src/main/java/pro/tyshchenko/nullhater/EmptyRef.java
@@ -2,9 +2,9 @@ package pro.tyshchenko.nullhater;
 /**
  * @author Alexander Tyshchenko
  */
-public final class NullRef {
+public final class EmptyRef {
 
-    public static <T> T justNull() {
+    public static <T> T justEmpty() {
         return null;
     }
 
